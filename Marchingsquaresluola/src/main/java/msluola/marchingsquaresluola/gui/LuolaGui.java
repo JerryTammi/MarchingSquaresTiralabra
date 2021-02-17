7e59ad2a9964349ba49e1c9ee548a2ad4d4239d6
JerryTammi <33928862+JerryTammi@users.noreply.github.com>
2021-02-17 16:11:59 +0200

    Turhan koodin poistamista

--- Marchingsquaresluola/src/main/java/msluola/marchingsquaresluola/gui/LuolaGui.java
+++ Marchingsquaresluola/src/main/java/msluola/marchingsquaresluola/gui/LuolaGui.java
@@ -58,7 +58,8 @@ public class LuolaGui {
                     luolienMaara = 2;
                 }
                 luoLuolasto(luolienMaara);
-                stage.setScene(luolastoScene);
+                stage.setScene(luolaScene);
+                stage.show();
             }
         });
         
@@ -67,43 +68,43 @@ public class LuolaGui {
     }
     
     public void luoLuolasto(int n) {
-        AnchorPane rootLuolasto = new AnchorPane();
         AnchorPane rootLuola = new AnchorPane();
+        luolaScene = new Scene(rootLuola, 2100, 1000);
         Pane utility = new Pane();
         utility.setLayoutX(35);
         utility.setLayoutY(25);
-        Button uusiLuolaNappi = new Button("Seuraava luola");
-        utility.getChildren().add(uusiLuolaNappi);
+        Button seuraavaLuolaNappi = new Button("Seuraava luola");
+        Button edellinnenLuolaNappi = new Button("Edellinen luola");
+        edellinnenLuolaNappi.setLayoutY(seuraavaLuolaNappi.getLayoutY() + 50);
+        utility.getChildren().addAll(seuraavaLuolaNappi, edellinnenLuolaNappi);
         SubScene s = new SubScene(utility, 200, 1000);
         s.setLayoutX(1920);
         
         Luolasto luolasto = new Luolasto(n);
-        luolasto.louLuolasto(1000, 1920, 20, rootLuolasto);
-        Button siirryLuoliin = new Button("Siirry ensimmäiseen luolaan");
-        siirryLuoliin.setLayoutX(25);
-        siirryLuoliin.setLayoutY(25);
+        luolasto.louLuolasto(1000, 1920, 20);
         nykyinenLuola = luolasto.haeLuolasto().get(0);
+        rootLuola.getChildren().addAll(nykyinenLuola.luoLuola(), s);
         
-        uusiLuolaNappi.setOnAction(ex -> {
+        seuraavaLuolaNappi.setOnAction(e -> {
             int index = luolasto.haeLuolasto().indexOf(nykyinenLuola);
             if (index < luolasto.haeLuolasto().size() - 1) {
                 Luola seuraavaLuola = luolasto.haeLuolasto().get(index + 1);
                 rootLuola.getChildren().remove(0);
                 rootLuola.getChildren().add(0, seuraavaLuola.luoLuola());
                 nykyinenLuola = seuraavaLuola;
-                stage.show();
             }
         });
         
-        siirryLuoliin.setOnAction(e -> {
-            luolaScene = new Scene(rootLuola, 2100, 1000);
-            rootLuola.getChildren().addAll(nykyinenLuola.luoLuola(), s);
-            stage.setScene(luolaScene);
-            stage.show();
-        });
-        
-        rootLuolasto.getChildren().add(siirryLuoliin);
-        luolastoScene = new Scene(rootLuolasto, 1920, 1000);
+//        edellinnenLuolaNappi.setOnAction(e -> {
+//            int index = luolasto.haeLuolasto().indexOf(nykyinenLuola);
+//            if (index > 0) {
+//                Luola edellinenLuola = luolasto.haeLuolasto().get(index - 1);
+//                rootLuola.getChildren().remove(0);
+//                rootLuola.getChildren().add(0, edellinenLuola.luoLuola());
+//                nykyinenLuola = edellinenLuola;
+//                
+//            }
+//        });
     }
     
     public Scene haeAsetusScene() {
--- Marchingsquaresluola/src/main/java/msluola/marchingsquaresluola/luola/Luolasto.java
+++ Marchingsquaresluola/src/main/java/msluola/marchingsquaresluola/luola/Luolasto.java
@@ -1,10 +1,7 @@
 package msluola.marchingsquaresluola.luola;
 
 import java.util.ArrayList;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import msluola.marchingsquaresluola.util.LehmerRng;
 
 public class Luolasto {
@@ -29,8 +26,7 @@ public class Luolasto {
      * @param vali algoritmeihin tarvittava luku, joka määrittää mm. kuinka tiheästi seinät ovat pakattu
      * @param p pane luolaston esittelyä varten
      */
-    public void louLuolasto(int korkeus, int leveys, int vali, AnchorPane p) {
-        this.pane = p;
+    public void louLuolasto(int korkeus, int leveys, int vali) {
         LehmerRng rand = new LehmerRng(System.nanoTime() % System.currentTimeMillis());
         long edellinen = 0;
         for (int i = 0; i < n; i++) {
@@ -42,34 +38,11 @@ public class Luolasto {
             luolat.add(l);
             int x = (vali * 5) + (int)(rand.lehmer() % (leveys - (vali * 10)));
             int y = (vali * 5) + (int)(rand.lehmer() % (korkeus - (vali * 10)));
-            lisaaPisteLuola(y, x, i, p, l);
             edellinen = seed;
         }
         lisaaJokaiselleLuolalleSeuraavaLuola();
     }
-    
-    /**
-     * Visualisoi luolien määrän. Tällä hetkellä satunnaisesti mutta tarkoituksena luoda parempi toteutus
-     * @param y luolan sijainnin y
-     * @param x luolan sijainnin x
-     * @param i tarvitaan ensimmäisen ja vikan luolan erotteluun
-     * @param p luolaston pane
-     * @param l yksittäinen luola luolastossa
-     */
-    public void lisaaPisteLuola(int y, int x, int i, AnchorPane p, Luola l) {
-        Circle c = new Circle(x, y, 8);
-        if (i == 0) {
-            c.setFill(Color.RED);
-        }
-        else if (i == (n - 1)) {
-            c.setFill(Color.GREEN);
-        }
-        else {
-            c.setFill(Color.BLACK);
-        }
-        p.getChildren().add(c);
-    }
-    
+        
     /**
      * Lisää jokaiselle luolalle muistiin seuraavan luolan numeron, jotta voidaan kulkea luolasta toiseen
      */
